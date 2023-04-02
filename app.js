944da7655de18d88f2dc0c49e4f4b94b20bfcc21
Catalist22 <yakovenko.seryoja@yandex.ru>
2023-04-02 20:23:59 +0300

    5

--- app.js
+++ app.js
@@ -3,3 +3,17 @@ const menu = document.querySelector("#menu");
 burger.addEventListener("click", () => {
     menu.classList.toggle("disp")
 });
+
+wow = new WOW({
+    animateClass: 'animated',
+    offset: 100,
+    callback: function(box) {
+    console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+    }
+    });
+    wow.init();
+    document.getElementById('moar').onclick = function() {
+    var section = document.createElement('section');
+    section.className = 'section--purple wow fadeInDown';
+    this.parentNode.insertBefore(section, this);
+    };
